19515f9469e3fbe9821f670f60763e5df0bc1b39
nirt <nirt@nirts-MBP.home>
2020-09-07 12:57:43 -0500

    Remove tokio

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,7 @@ use game::*;
 use druid::widget::{Button, Flex, Label};
 use druid::{AppLauncher, LocalizedString, Env, PlatformError, Widget, WidgetExt, WindowDesc};
 
-#[tokio::main]
-async fn main() -> Result<(), PlatformError>{
+fn main() -> Result<(), PlatformError> {
     println!("Starting...");
     let main_window = WindowDesc::new(game_window);
     AppLauncher::with_window(main_window)
